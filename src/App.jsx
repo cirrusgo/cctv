936047e4977b9957139eedc26824762824296c4e
Obada <obada.qafisheh@cirrusgo.com>
2022-08-16 09:34:30 +0300

    sec deploy

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,8 @@ function App({ signOut, user }) {
     API: {
       'aws_appsync_graphqlEndpoint':'https://n5msff4dnbhyljjfkmeagnkqlq.appsync-api.us-east-1.amazonaws.com/graphql',
       'aws_appsync_region': 'us-east-1',
-      'aws_appsync_authenticationType': 'AMAZON_COGNITO_USER_POOLS'
+      'aws_appsync_authenticationType': 'API_KEY',
+      'aws_appsync_apiKey': 'da2-bvbra6xeave5tfo7xluauc5adi'
     }
   })
 
